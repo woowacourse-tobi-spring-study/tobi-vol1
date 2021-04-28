da698a75ef820cca8cb1c466bda76fde90e1d96f
김주원 <kjw11077@naver.com>
2021-04-29 01:03:31 +0900

    feat: 1.1 초난감 DAO

---
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -0,0 +1,52 @@
+package springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import springbook.user.domain.User;
+
+public class UserDao {
+
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager
+                .getConnection("jdbc:mysql://localhost/tobi_db", "root", "root");
+
+        PreparedStatement ps = c.prepareStatement(
+                "INSERT INTO users(id, name, password) VALUES (?, ?, ?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager
+                .getConnection("jdbc:mysql://localhost/tobi_db", "root", "root");
+
+        PreparedStatement ps = c.prepareStatement(
+                "SELECT * FROM users WHERE id = ?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+}
