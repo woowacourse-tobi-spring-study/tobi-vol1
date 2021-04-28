d99e574171d4720582b512e35762075851971b37
Sung Si Hyung <fjzjqhdl@gmail.com>
2021-04-28 12:13:36 +0900

    feat : UserDao를 검증하는 테스트 작성

---
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -0,0 +1,30 @@
+package springbook.user.dao;
+
+import org.junit.jupiter.api.Test;
+import springbook.user.domain.User;
+
+import java.sql.SQLException;
+
+class UserDaoTest {
+
+    @Test
+    void add() throws SQLException, ClassNotFoundException {
+        //given
+        UserDao userDao = new UserDao();
+
+        User user = new User();
+        user.setId("1");
+        user.setName("wedge");
+        user.setPassword("wedge123");
+
+        //when
+        userDao.add(user);
+        System.out.println(user.getId() + "등록 성공");
+
+        User user2 = userDao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+        System.out.println(user2.getId() + "조회 성공");
+        //then
+    }
+}
