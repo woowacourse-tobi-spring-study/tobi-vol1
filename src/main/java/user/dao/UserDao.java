b54c3fc1789d1307d7f5bf6a2e5b5be24d66c98e
xrabcde <xrabcde@gmail.com>
2021-04-29 19:10:20 +0900

    feat: 초난감 DAO 구현

---
+++ src/main/java/user/dao/UserDao.java
@@ -0,0 +1,45 @@
+package user.dao;
+
+import user.domain.User;
+
+import java.sql.*;
+
+public class UserDao {
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("org.h2.Driver");
+        Connection con = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/springbook", "sa", "");
+
+        PreparedStatement ps = con.prepareStatement(
+            "insert into users(id, name, password) values(?, ?, ?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        con.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("org.h2.Driver");
+        Connection con = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/springbook", "sa", "");
+
+        PreparedStatement ps = con.prepareStatement(
+            "select * from users where id = ?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        con.close();
+
+        return user;
+    }
+}
