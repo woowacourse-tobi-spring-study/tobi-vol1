5c4b3821e3a0ac06386be8e1f91b39cf4dcfb428
thisisyoungbin <thisisyoungbin@gmail.com>
2021-05-01 15:11:11 +0900

    ~ p94까지 구현

---
+++ src/main/java/Application.java
@@ -0,0 +1,10 @@
+import dao.UserDao;
+import domain.User;
+
+import java.sql.SQLException;
+
+public class Application {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+    }
+}
---
+++ src/main/java/dao/ConnectionMaker.java
@@ -0,0 +1,8 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface ConnectionMaker {
+    public Connection makeConnection() throws ClassNotFoundException, SQLException;
+}
