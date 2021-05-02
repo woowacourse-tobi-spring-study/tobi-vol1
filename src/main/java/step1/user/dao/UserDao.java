02cad9b3a081d3ddfbdd61c10d77436806b37154
Minsung <sns2831@gmail.com>
2021-05-03 01:22:35 +0900

    refactor: userDao 중복 코드 제거

--- src/main/java/step1/user/dao/UserDao.java
+++ src/main/java/step1/user/dao/UserDao.java
@@ -7,9 +7,7 @@ import java.sql.*;
 public class UserDao {
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc.mysql:://localhost:13306/springbook", "spring", "book");
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement(
                 "insert into users(id, name, password) values(?,?,?)");
@@ -25,9 +23,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc.mysql:://localhost:13306/springbook", "spring", "book");
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement(
                 "select * from users where id = ?");
@@ -45,6 +41,12 @@ public class UserDao {
         c.close();
 
         return user;
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection(
+                "jdbc.mysql:://localhost:13306/springbook", "spring", "book");
     }
 
 }
