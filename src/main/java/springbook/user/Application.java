b0a3f839f99a30e2e35ecd265a7d0e452566b2a8
younghoonkwon <younghoon.kwon91@gmail.com>
2021-05-02 16:21:08 +0900

    refactor: userDao 관련 관심사 주석으로 추가

---
+++ src/main/java/springbook/user/Application.java
@@ -0,0 +1,27 @@
+package springbook.user;
+
+import springbook.user.dao.UserDao;
+import springbook.user.domain.User;
+
+import java.sql.SQLException;
+
+public class Application {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("백기선");
+        user.setPassword("married");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + " 등록 성공");
+    }
+}
--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -5,6 +5,12 @@ import springbook.user.domain.User;
 import java.sql.*;
 
 public class UserDao {
+    /*
+    관심사
+    DB와 연결을 위한 커넥션
+    사용자 등록을 위해 DB에 보낼 Statement를 만들고 실행
+    커넥션을 닫아주는 것
+     */
     public void add(User user) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
         Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "spring", "book");
