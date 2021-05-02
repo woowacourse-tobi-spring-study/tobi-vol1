3a15fbadd7456a1d31eb0227ce5b0d9c4aa6d693
bomin <bomin_93@naver.com>
2021-05-02 21:53:08 +0900

    feat(dao): 엉망인 dao

---
+++ src/main/java/user/dao/chaos/UserDao.java
@@ -0,0 +1,49 @@
+package user.dao.chaos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import user.domain.User;
+
+public class UserDao {
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/nabom", "nabom", "1234");
+
+        final PreparedStatement preparedStatement = connection.prepareStatement(
+            "insert into users(id, name, password) values(?,?,?)"
+        );
+        preparedStatement.setString(1, user.getId());
+        preparedStatement.setString(2, user.getName());
+        preparedStatement.setString(3, user.getPassword());
+
+        preparedStatement.executeUpdate();
+
+        preparedStatement.close();
+        connection.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/nabom", "nabom", "1234");
+
+        final PreparedStatement preparedStatement = connection.prepareStatement(
+            "select * from users where id = ?"
+        );
+        preparedStatement.setString(1, id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        User user = new User();
+        user.setId(resultSet.getString("id"));
+        user.setName(resultSet.getString("name"));
+        user.setPassword(resultSet.getString("password"));
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+        return user;
+    }
+}
