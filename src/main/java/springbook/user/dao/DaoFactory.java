d9ec5f63cdf734deb29856cd0257da9836151972
Gomding <backchida@gmail.com>
2021-05-02 12:00:14 +0900

    refactor : DaoFactory 클래스에서 Connection 생성 메서드도 분리

--- src/main/java/springbook/user/dao/DaoFactory.java
+++ src/main/java/springbook/user/dao/DaoFactory.java
@@ -2,8 +2,12 @@ package springbook.user.dao;
 
 public class DaoFactory {
     public static UserDao userDao() {
-        ConnectionMaker connectionMaker = new SimpleConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
+        UserDao userDao = new UserDao(simpleConnectionMaker());
         return userDao;
+    }
+
+    //TODO Connection 도 팩터리 클래스를 만든다면?
+    private static ConnectionMaker simpleConnectionMaker() {
+        return new SimpleConnectionMaker();
     }
 }
