14190fea46c45af52532291a0a414a178d9b9caa
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2021-05-02 17:10:59 +0900

    refactor: ConnectionMaker와 UserDao를 생성하는 로직을 담당하는 DaoFactory 추가

--- src/main/java/springbook/UserDaoTest.java
+++ src/main/java/springbook/UserDaoTest.java
@@ -1,7 +1,6 @@
 package springbook;
 
-import springbook.user.dao.ConnectionMaker;
-import springbook.user.dao.DConnectionMaker;
+import springbook.user.dao.DaoFactory;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
@@ -9,8 +8,7 @@ import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-        UserDao dao = new UserDao(connectionMaker);
+        UserDao dao = new DaoFactory().userDao();
 
         User user = new User();
         user.setId("whiteship");
