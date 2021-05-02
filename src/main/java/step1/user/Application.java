0ed2f45260e5767e7e3e6238c1df3e6982d02cd8
Minsung <sns2831@gmail.com>
2021-05-03 01:21:12 +0900

    feat: userDao 테스트 코드 작성

---
+++ src/main/java/step1/user/Application.java
@@ -0,0 +1,29 @@
+package step1.user;
+
+import step1.user.dao.UserDao;
+import step1.user.domain.User;
+
+import java.sql.SQLException;
+
+public class Application {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("백기선");
+        user.setPassword("married");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+        System.out.println(user2.getId() + " 조회 성공");
+    }
+
+}
