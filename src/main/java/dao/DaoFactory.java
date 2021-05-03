3434d46845e98624d8c46cdfd499a7dc7db7e662
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-05-03 14:25:40 +0900

    refactor: configuration, bean 어노테이션 추가

--- src/main/java/dao/DaoFactory.java
+++ src/main/java/dao/DaoFactory.java
@@ -1,11 +1,18 @@
 package dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+
+    @Bean
     public UserDao userDao() {
         return new UserDao(connectionMaker());
     }
 
-    private ConnectionMaker connectionMaker() {
+    @Bean
+    public ConnectionMaker connectionMaker() {
         return new DConnectionMaker();
     }
 }
