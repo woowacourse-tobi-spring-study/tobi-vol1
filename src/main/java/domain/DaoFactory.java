bf23bfa90acab61670268817fe6777a2eeb25811
yjksw <cairofaith@gmail.com>
2021-05-06 20:33:47 +0900

    feat: 스프링 IoC를 사용한 UserDao 주입

--- src/main/java/domain/DaoFactory.java
+++ src/main/java/domain/DaoFactory.java
@@ -3,15 +3,20 @@ package domain;
 import dao.ConnectionMaker;
 import dao.MysqlConnection;
 import dao.UserDao;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class DaoFactory {
 
+    @Bean
     public UserDao userDao() {
         UserDao dao = new UserDao(connectionMaker());
         return dao;
     }
 
     //추후 여러 DAO가 만들었을 때 중복 제거를 위한 메소드 분리
+    @Bean
     public ConnectionMaker connectionMaker() {
         return new MysqlConnection();
     }
