188ff98030543420d65505624149ed46382e7c2d
Sung Si Hyung <fjzjqhdl@gmail.com>
2021-05-07 21:24:21 +0900

    feat : dataSource와 UserDao객체를 XML로 DI받아 사용

--- src/test/java/springbook/user/dao/UserDaoTest.java
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -3,6 +3,7 @@ package springbook.user.dao;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import springbook.user.domain.User;
 
 import java.sql.SQLException;
@@ -12,7 +13,7 @@ class UserDaoTest {
     @Test
     void add() throws SQLException, ClassNotFoundException {
         //given
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         UserDao userDao = context.getBean("UserDao", UserDao.class);
 
         userDao.delete();
