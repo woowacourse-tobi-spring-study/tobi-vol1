8369cd8cbdcf1ca6adcc6ae888cbdc9b9d4c3d5d
Daeun Lee <leede0418@likelion.org>
2021-05-09 22:19:48 +0900

    refactor: 어플리케이션 컨텍스트가 사용할 설정정보로 변환

--- src/main/java/springbook/dao/DaoFactory.java
+++ src/main/java/springbook/dao/DaoFactory.java
@@ -1,10 +1,16 @@
 package springbook.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao() {
         return new UserDao(connectionMaker());
     }
 
+    @Bean
     public ConnectionMaker connectionMaker() {
         return new SimpleConnectionMaker();
     }
