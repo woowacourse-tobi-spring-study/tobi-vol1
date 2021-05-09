f30ac5453fb85c9b2fe0752ccdcbc9e00ec93f54
Daeun Lee <leede0418@likelion.org>
2021-05-09 22:20:22 +0900

    refactor: 어플리케이션 컨텍스트 적용

--- src/main/java/springbook/dao/UserDaoTest.java
+++ src/main/java/springbook/dao/UserDaoTest.java
@@ -1,12 +1,15 @@
 package springbook.dao;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import springbook.domain.User;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        UserDao dao = new DaoFactory().userDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
         user.setId("whiteship");
