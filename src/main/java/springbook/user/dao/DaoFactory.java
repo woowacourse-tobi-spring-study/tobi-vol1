160487f415557d90f7ef50d46b52cb679f143e69
Gomding <backchida@gmail.com>
2021-05-10 22:32:15 +0900

    feat : BeanFactory에 @Configuration 을 추가해서 애플리케이션 또는 빈 팩토리의 설정파일로 구현

--- src/main/java/springbook/user/dao/DaoFactory.java
+++ src/main/java/springbook/user/dao/DaoFactory.java
@@ -1,13 +1,20 @@
 package springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+
+    @Bean
     public static UserDao userDao() {
-        UserDao userDao = new UserDao(simpleConnectionMaker());
+        UserDao userDao = new UserDao(connectionMaker());
         return userDao;
     }
 
     //TODO Connection 도 팩터리 클래스를 만든다면?
-    private static ConnectionMaker simpleConnectionMaker() {
+    @Bean
+    public static ConnectionMaker connectionMaker() {
         return new SimpleConnectionMaker();
     }
 }
