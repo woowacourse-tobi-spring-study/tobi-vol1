1a0a253bc42066068557044bb3a8e92aa6d6e72a
younghoonkwon <younghoon.kwon91@gmail.com>
2021-05-10 23:57:53 +0900

    feat: schema 업데이트

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+create database if not exists tobi
+DEFAULT CHARACTER SET utf8
+    COLLATE utf8_general_ci;
+
+use tobi;
+
+create table if not exists users
+(
+    id   varchar(255) 		not null unique,
+    name varchar(255)          not null unique,
+    password varchar(255) not null,
+    primary key (id)
+);
