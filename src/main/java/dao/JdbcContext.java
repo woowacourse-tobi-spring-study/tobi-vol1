634e8cd529fc1fcd89b9ee9a2a37e7d85680709a
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-05-16 21:47:00 +0900

    refactor: 템플릿/콜백 패턴으로 변경

---
+++ src/main/java/dao/JdbcContext.java
@@ -0,0 +1,55 @@
+package dao;
+
+import dao.strategy.StatementStrategy;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class JdbcContext {
+    private final DataSource dataSource;
+
+    public JdbcContext(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void executeSql(final String query) throws SQLException {
+        workWithStatementStrategy(
+                c ->  c.prepareStatement(query)
+        );
+    }
+
+    public void executeVarArgsSql(final String query, String... args) throws SQLException {
+        workWithStatementStrategy(
+                c -> c.prepareStatement(query, args)
+        );
+    }
+
+    private void workWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+
+        try {
+            c = this.dataSource.getConnection();
+            ps = stmt.makePreparedStatement(c);
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                }
+            }
+        }
+    }
+}
--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -6,57 +6,30 @@ import dao.strategy.StatementStrategy;
 import domain.User;
 
 import javax.sql.DataSource;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UserDao {
+    private JdbcContext jdbcContext;
     private DataSource dataSource;
 
     public UserDao(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
+    public void setJdbcContext(JdbcContext jdbcContext) {
+        this.jdbcContext = jdbcContext;
+    }
+
     public void add(User user) throws SQLException {
-        StatementStrategy st = new AddStatement(user);
-        jdbcContextWithStatementStrategy(
-                c -> {
-                    PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values (?,?,?)");
-                    ps.setString(1, user.getId());
-                    ps.setString(2, user.getId());
-                    ps.setString(3, user.getId());
-                    return ps;
-                }
-        );
+        jdbcContext.executeVarArgsSql("insert into users(id, name, password values (?,?,?)",
+                user.getId(), user.getName(), user.getPassword());
     }
 
     public void deleteAll() throws SQLException {
-        StatementStrategy st = new DeleteAllStatement();
-        jdbcContextWithStatementStrategy(st);
-    }
-
-    private void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-
-        try {
-            c = dataSource.getConnection();
-            ps = stmt.makePreparedStatement(c);
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                }
-            }
-        }
+        jdbcContext.executeSql("delete from users");
     }
 
     public User get(String id) throws SQLException {
