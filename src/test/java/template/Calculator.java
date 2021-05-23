15d23d99be7d3fb92f52cd73648d0908ba66084f
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-05-23 12:20:34 +0900

    feat(Calculator): 템플릿/콜백을 통한 곱셈 기능 구현

--- src/test/java/template/Calculator.java
+++ src/test/java/template/Calculator.java
@@ -6,16 +6,27 @@ import java.io.IOException;
 
 public class Calculator {
     public int calcSum(String path) throws IOException {
-        BufferedReaderCallback sumCallback =
-                br -> {
-                    int sum = 0;
-                    String line = null;
-                    while ((line = br.readLine()) != null) {
-                        sum += Integer.valueOf(line);
-                    }
-                    return sum;
-                };
+        BufferedReaderCallback sumCallback = br -> {
+            int sum = 0;
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                sum += Integer.valueOf(line);
+            }
+            return sum;
+        };
         return fileReadTemplate(path, sumCallback);
+    }
+
+    public int calcMultiply(String path) throws IOException {
+        BufferedReaderCallback multiCallback = br -> {
+            int multi = 1;
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                multi *= Integer.valueOf(line);
+            }
+            return multi;
+        };
+        return fileReadTemplate(path, multiCallback);
     }
 
     private Integer fileReadTemplate(String path, BufferedReaderCallback callback) throws IOException {
--- src/test/java/template/CalculatorTest.java
+++ src/test/java/template/CalculatorTest.java
@@ -1,5 +1,6 @@
 package template;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -7,11 +8,24 @@ import java.io.IOException;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
+    private Calculator calculator;
+    private String path;
+
+    @BeforeEach
+    public void setUp() {
+        calculator = new Calculator();
+        path = getClass().getClassLoader().getResource("numbers.txt").getPath();
+    }
+
     @Test
     public void sum() throws IOException {
-        Calculator calculator = new Calculator();
-        final String path = getClass().getClassLoader().getResource("numbers.txt").getPath();
         int sum = calculator.calcSum(path);
         assertThat(sum).isEqualTo(10);
+    }
+
+    @Test
+    public void multiply() throws IOException {
+        int multiply = calculator.calcMultiply(path);
+        assertThat(multiply).isEqualTo(24);
     }
 }
