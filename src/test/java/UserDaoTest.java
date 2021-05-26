028663833ff02929b99acb31846cd4e3c439f867
xrabcde <xrabcde@gmail.com>
2021-05-26 17:22:23 +0900

    refactor(UserDaoTest): 테스트 검증 자동화

--- src/test/java/UserDaoTest.java
+++ src/test/java/UserDaoTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import user.dao.DaoFactory;
@@ -7,11 +8,9 @@ import user.domain.User;
 
 import java.sql.SQLException;
 
-
 public class UserDaoTest {
 
-    @Test
-    void test() throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
         UserDao dao = context.getBean("userDao", UserDao.class);
 
@@ -25,9 +24,12 @@ public class UserDaoTest {
         System.out.println(user.getId() + "등록 성공");
 
         User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
-
-        System.out.println(user2.getId() + "조회 성공");
+        if (!user.getName().equals(user2.getName())) {
+            System.out.println("테스트 실패 (name)");
+        } else if (!user.getPassword().equals(user2.getPassword())) {
+            System.out.println("테스트 실패 (password)");
+        } else {
+            System.out.println("조회 테스트 성공");
+        }
     }
 }
