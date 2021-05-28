4fe336b527e7749685f2c65b325e097ab02ecce6
xrabcde <xrabcde@gmail.com>
2021-05-29 00:10:22 +0900

    test: getCount()에 대한 테스트 코드 작성

--- src/test/java/UserDaoTest.java
+++ src/test/java/UserDaoTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import user.dao.DaoFactory;
 import user.dao.UserDao;
 import user.domain.User;
@@ -28,5 +29,28 @@ public class UserDaoTest {
         User user2 = dao.get(user.getId());
         assertThat(user2.getName(), is(user.getName()));
         assertThat(user2.getPassword(), is(user.getPassword()));
+    }
+
+    @Test
+    void count() throws SQLException {
+        ApplicationContext context = new GenericXmlApplicationContext (
+            "applicationContext.xml");
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        User user1 = new User("xrabcde", "조아라", "1234");
+        User user2 = new User("xrabcd", "조아", "1234");
+        User user3 = new User("xrabc", "조", "1234");
+
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
+        dao.add(user1);
+        assertThat(dao.getCount(), is(1));
+
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
+
+        dao.add(user3);
+        assertThat(dao.getCount(), is(3));
     }
 }
