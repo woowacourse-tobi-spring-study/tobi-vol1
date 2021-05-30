ebb5b41e06487e878fa19c73367e6446a47f5aab
Gomding <backchida@gmail.com>
2021-05-31 00:58:37 +0900

    feat : 테스트에서 ApplicationContext를 빈으로 주입받도록 @Autowired를 사용

---
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -0,0 +1,83 @@
+package springbook.user.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import springbook.user.User;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringJUnitConfig
+@ContextConfiguration(locations = "/applicationContext.xml")
+class UserDaoTest {
+    @Autowired
+    ApplicationContext context;
+
+    UserDao userDao;
+    User user1;
+    User user2;
+    User user3;
+
+    @BeforeEach
+    void setUp() {
+        userDao = context.getBean("userDao", UserDao.class);
+        user1 = new User("gyumee", "박성철", "springno1");
+        user2 = new User("leegw700", "이길원", "springno2");
+        user3 = new User("bumjin", "박범진", "springno3");
+    }
+
+    @Test
+    public void count() throws SQLException {
+        User user1 = new User("gyumee", "박성철", "springno1");
+        User user2 = new User("leegw700", "이길원", "springno2");
+        User user3 = new User("bumjin", "박범진", "springno3");
+
+        userDao.deleteAll();
+        assertEquals(userDao.getCount(), 0);
+
+        userDao.add(user1);
+        assertEquals(userDao.getCount(), 1);
+
+        userDao.add(user2);
+        assertEquals(userDao.getCount(), 2);
+
+        userDao.add(user3);
+        assertEquals(userDao.getCount(), 3);
+    }
+
+    @Test
+    public void addAndGet() throws SQLException {
+        User user1 = new User("gyumee", "박성철", "springno1");
+        User user2 = new User("leegw700", "이길원", "springno2");
+
+        userDao.deleteAll();
+        assertEquals(userDao.getCount(), 0);
+
+        userDao.add(user1);
+        userDao.add(user2);
+        assertEquals(userDao.getCount(), 2);
+
+        User userGet1 = userDao.get(user1.getId());
+        assertEquals(userGet1.getName(), user1.getName());
+        assertEquals(userGet1.getPassword(), user1.getPassword());
+
+        User userGet2 = userDao.get(user2.getId());
+        assertEquals(userGet2.getName(), user2.getName());
+        assertEquals(userGet2.getPassword(), user2.getPassword());
+    }
+
+    @Test
+    public void getUserFailure() throws SQLException {
+        userDao.deleteAll();
+        assertEquals(userDao.getCount(), 0);
+
+        assertThrows(EmptyResultDataAccessException.class, () -> userDao.get("unknown_id"));
+    }
+}
