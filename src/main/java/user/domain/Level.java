786c1b68b9d9b46e124b3445e1c39baf6d4d4422
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-06-13 22:32:35 +0900

    feat(Level): nextLevel 메서드 추가

--- src/main/java/user/domain/Level.java
+++ src/main/java/user/domain/Level.java
@@ -1,24 +1,36 @@
 package user.domain;
 
 public enum Level {
-    BASIC(1), SILVER(2), GOLD(3);
+    GOLD(3, null),
+    SILVER(2, GOLD),
+    BASIC(1, SILVER);
 
     private final int value;
+    private final Level nextLevel;
 
-    Level(int value) {
+    Level(int value, Level nextLevel) {
         this.value = value;
+        this.nextLevel = nextLevel;
     }
 
     public int intValue() {
         return value;
     }
 
+    public Level nextLevel() {
+        return nextLevel;
+    }
+
     public static Level valueOf(int value) {
-        switch(value) {
-            case 1: return BASIC;
-            case 2: return SILVER;
-            case 3: return GOLD;
-            default: throw new AssertionError("unknown value" + value);
+        switch (value) {
+            case 1:
+                return BASIC;
+            case 2:
+                return SILVER;
+            case 3:
+                return GOLD;
+            default:
+                throw new AssertionError("unknown value" + value);
         }
     }
 }
