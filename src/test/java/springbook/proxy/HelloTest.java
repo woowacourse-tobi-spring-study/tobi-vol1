ab92d0e7b65a1675ec3bc3316b0085dac0c448cb
Jinhong <xlffm3@gmail.com>
2021-06-18 01:30:34 +0900

    test: pointcut 클래스 필터링 학습 테스트

--- src/test/java/springbook/proxy/HelloTest.java
+++ src/test/java/springbook/proxy/HelloTest.java
@@ -3,6 +3,7 @@ package springbook.proxy;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.ClassFilter;
 import org.springframework.aop.framework.ProxyFactoryBean;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
@@ -57,6 +58,52 @@ class HelloTest {
         assertThat(proxy.sayHello("abc")).isEqualTo("HELLOABC");
         assertThat(proxy.sayHi("abc")).isEqualTo("HIABC");
         assertThat(proxy.sayThankYou("abc")).isNotEqualTo("THANK YOUABC");
+    }
+
+    @Test
+    void classNamePointcutAdvisor() {
+        NameMatchMethodPointcut nameMatchMethodPointcut = new NameMatchMethodPointcut() {
+            @Override
+            public ClassFilter getClassFilter() {
+                return new ClassFilter() {
+                    @Override
+                    public boolean matches(Class<?> clazz) {
+                        return clazz.getSimpleName().startsWith("HelloT");
+                    }
+                };
+            }
+        };
+        nameMatchMethodPointcut.setMappedName("sayH*");
+
+        checkAdviced(new HelloTarget(), nameMatchMethodPointcut, true);
+        checkAdviced(new HelloWorld(), nameMatchMethodPointcut, false);
+        checkAdviced(new HelloToby(), nameMatchMethodPointcut, true);
+
+    }
+
+    private void checkAdviced(Hello hello, NameMatchMethodPointcut nameMatchMethodPointcut, boolean isAdviced) {
+        ProxyFactoryBean proxyFactoryBean = new ProxyFactoryBean();
+        proxyFactoryBean.setTarget(hello);
+        proxyFactoryBean.addAdvisor(new DefaultPointcutAdvisor(nameMatchMethodPointcut, new UppercaseAdvice()));
+        Hello proxy = (Hello) proxyFactoryBean.getObject();
+
+        if (isAdviced) {
+            assertThat(proxy.sayHello("abc")).isEqualTo("HELLOABC");
+            assertThat(proxy.sayHi("abc")).isEqualTo("HIABC");
+            assertThat(proxy.sayThankYou("abc")).isNotEqualTo("THANK YOUABC");
+        } else {
+            assertThat(proxy.sayHello("abc")).isNotEqualTo("HELLOABC");
+            assertThat(proxy.sayHi("abc")).isNotEqualTo("HIABC");
+            assertThat(proxy.sayThankYou("abc")).isNotEqualTo("THANK YOUABC");
+        }
+    }
+
+
+    static class HelloWorld extends HelloTarget {
+    }
+
+
+    static class HelloToby extends HelloTarget {
     }
 
     static class UppercaseAdvice implements MethodInterceptor {
