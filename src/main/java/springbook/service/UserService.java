28cc08f4ab5c68328e456eb29af1fec234c8e9fc
Jinhong <xlffm3@gmail.com>
2021-06-18 05:26:15 +0900

    refactor: @transactional 애너테이션 기반 수정

--- src/main/java/springbook/service/UserService.java
+++ src/main/java/springbook/service/UserService.java
@@ -1,17 +1,21 @@
 package springbook.service;
 
+import org.springframework.transaction.annotation.Transactional;
 import springbook.domain.user.User;
 
 import java.util.List;
 
+@Transactional
 public interface UserService {
 
     void upgradeLevels();
 
     void add(User user);
 
+    @Transactional(readOnly = true)
     User get(String id);
 
+    @Transactional(readOnly = true)
     List<User> getAll();
 
     void deleteAll();
