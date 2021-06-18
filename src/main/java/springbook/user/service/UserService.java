cd186fabe5232eb36a565b139561d3b3641382d7
Gomding <backchida@gmail.com>
2021-06-18 22:43:49 +0900

    feat : 레벨 업그레이드시 안내 메일 발송 기능 추가

--- src/main/java/springbook/user/service/UserService.java
+++ src/main/java/springbook/user/service/UserService.java
@@ -8,7 +8,14 @@ import springbook.user.User;
 import springbook.user.UserLevelUpgradePolicy;
 import springbook.user.dao.UserDao;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.util.List;
+import java.util.Properties;
 
 public class UserService {
     private UserDao userDao;
@@ -46,6 +53,25 @@ public class UserService {
             userLevelUpgradePolicy.upgradeLevel(user);
         }
         userDao.update(user);
+        sendUpgradeEMail(user);
+    }
+
+    private void sendUpgradeEMail(User user) {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "mail.ksug.org");
+        Session session = Session.getInstance(props, null);
+
+        MimeMessage message = new MimeMessage(session);
+        try {
+            message.setFrom(new InternetAddress("useradmin@ksug.org"));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(user.getEmail()));
+            message.setSubject("Upgrade 안내");
+            message.setText("사용자님의 등급이 " + user.getLevel().name() + "로 업그레이드되었습니다.");
+
+            Transport.send(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void add(User user) {
